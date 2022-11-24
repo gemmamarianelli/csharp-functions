516ce8073db4a596f1216b36feb2610c8a5f222b
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-11-24 17:24:53 +0100

    Add project files.

---
+++ csharp-functions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+int[] arrayNumeri = { 2, 6, 7, 5, 3, 9 };
+StampaArrayDiNumeri(arrayNumeri);
+
+
+ float sum = arrayNumeri.Sum();
+Console.WriteLine(sum.ToString());
+Console.Write("[");
+Console.Write(Math.Pow(sum, 2));
+Console.Write("]");
+
+for(int i = 0; i<arrayNumeri.Length; i++)
+{
+    Console.Write("[");
+    Console.Write(Math.Pow(arrayNumeri[i], 2));
+    Console.Write("]");
+    
+}
+
+
+
+
+
+//--------------------------FUNZIONI-----------------------------
+
+void StampaArrayDiNumeri(int[] arrayNumeri)
+{
+    Console.Write("[");
+    for (int i=0; i<arrayNumeri.Length; i++)
+    {
+        Console.Write(arrayNumeri[i] + ",");
+    }
+    Console.Write("]");
+   
+}
+
